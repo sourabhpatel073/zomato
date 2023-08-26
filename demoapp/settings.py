854263843f1fcac6582d87190c0fb66b33618ca5
Sourabh Patel <sourabhpatel073@gmail.com>
2023-08-26 18:39:27 +0530

    render

--- demoapp/settings.py
+++ demoapp/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+PORT = os.getenv("PORT", default="8000")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-c(ie92g6#t8bfbvve8x=r#ia9pq&@t7mh^h&5&y(-_ttax)_w^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['demoapp-rosy.vercel.app', 'localhost', '127.0.0.1','.']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
